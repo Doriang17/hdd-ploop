dcf80aee08d6df56946abe6d1042754a16cc1e2f
Dorian <dorian@cathost.fr>
2016-04-04 02:43:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ yum upgrade
 wget -P /etc/yum.repos.d/ http://ftp.openvz.org/openvz.repo
 rpm --import http://ftp.openvz.org/RPM-GPG-Key-OpenVZ
 yum update
-yum install vzkernel
-yum install ploop vzctl vzquota
+yum install epel-release vzkernel
+yum install ploop vzctl vzquota pigz
 echo "SELINUX=disabled" > /etc/sysconfig/selinux
+cd /home
+wget https://github.com/Doriang17/hdd-ploop/blob/master/dd.sh
 echo "Votre kernel openvz est maintenant opérationel veuillez redemarrer le serveur via reboot puis executer dd.sh dans le /home Appuyez sur entrée pour quitter"
 
 read test
